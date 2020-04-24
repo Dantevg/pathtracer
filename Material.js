4ccaf5a4a4caf98c38d57ade678aad5bd31c4a8f
Dantevg <mail@dantevg.nl>
2020-04-24 19:46:33 +0200

    Divide ray bounce into specular, diffuse and transmit

--- Material.js
+++ Material.js
@@ -6,19 +6,11 @@ class Material {
 		this.emission = emission ?? 0
 	}
 	
-	bounce( ray ){
-		const normal = ray.to.normal
-		let direction
-		
-		const colour = Colour.multiply( ray.colour, ray.to.object.colour, ray.colour.a ) // Only for ray visualising
-		
-		// TODO: use proprer measure of reflectance vs transmittance
-		if( Math.random() >= this.transparency ){ // Reflect
-			direction = Vector.subtract( ray.dir, Vector.multiply( normal, 2*normal.dot(ray.dir) ) )
-		}else{ // Transmit
-			direction = ray.dir
-		}
-		
+	specular( ray ){
+		return Vector.subtract( ray.dir, Vector.multiply( ray.to.normal, 2*ray.to.normal.dot(ray.dir) ) )
+	}
+	
+	diffuse( ray ){
 		const dir = new Vector( Math.random()*2-1, Math.random()*2-1 )
 		while( dir.dot( dir ) > 1 ){
 			dir.set( Math.random()*2-1, Math.random()*2-1 )
@@ -27,7 +19,25 @@ class Material {
 		
 		// Ensure ray goes in right direction
 		dir.multiply( -Math.sign( ray.dir.dot( ray.to.normal ) * dir.dot( ray.to.normal ) ) )
-		return new Ray( ray.to.point, dir.toAngles(), ray.depth-1, colour )
+		return dir
+	}
+	
+	transmit( ray ){
+		
+	}
+	
+	bounce( ray ){
+		const normal = ray.to.normal
+		let direction
+		
+		const colour = Colour.multiply( ray.colour, ray.to.object.colour, ray.colour.a ) // Only for ray visualising
+		
+		// TODO: use proprer measure of reflectance vs transmittance
+		if( Math.random() >= this.transparency ){ // Reflect
+			return new Ray( ray.to.point, this.diffuse(ray), ray.depth-1, colour )
+		}else{ // Transmit
+			return new Ray( ray.to.point, ray.dir.toAngles(), ray.depth-1, colour )
+		}
 	}
 	
 	// Some default materials
@@ -35,7 +45,7 @@ class Material {
 		return new Material({roughness: 0})
 	}
 	static get matte(){
-		return new Material({roughness: 1})
+		return new Material({roughness: 0.2})
 	}
 	static get transmissive(){
 		return new Material({roughness: 0, transparency: 1})
