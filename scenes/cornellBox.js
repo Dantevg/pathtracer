9d885e177ca9b2c88205ea0d6ccc0ba39501eb3c
Dantevg <dantevg006@gmail.com>
2020-07-22 15:51:32 +0200

    Add some more testing shapes, change scene loading

--- scenes/cornellBox.js
+++ scenes/cornellBox.js
@@ -6,6 +6,8 @@ import Material from "../Material.js"
 import Sphere from "../shapes/Sphere.js"
 import Plane from "../shapes/Plane.js"
 import Triangle from "../shapes/Triangle.js"
+import Cube from "../shapes/Cube.js"
+import TriangleMesh from "../shapes/TriangleMesh.js"
 
 const scene = new Scene()
 
@@ -28,8 +30,19 @@ scene.objects.push( new Sphere( new Vector(75, 75, 50), 25, Colour.WHITE, Materi
 // Triangle
 // scene.objects.push( new Triangle( new Vector(-100, -100, -100), new Vector(100, 100, -100), new Vector(-100, 100, -100), Colour.WHITE, Material.matte ) )
 
+// Cube
+// scene.objects.push( new Cube( new Vector(0, 0, -50), new Vector(1, 0, 0), new Vector(0, 1, 1), 100, Colour.YELLOW, Material.matte ) )
+
+// Triangle mesh
+// scene.objects.push( new TriangleMesh(
+// 	[new Vector(-100, -100, -100), new Vector(100, -100, -100), new Vector(-100, 100, -100), new Vector(100, 100, -100),
+// 	new Vector(-100, -100, 100), new Vector(100, -100, 100), new Vector(-100, 100, 100), new Vector(100, 100, 100)],
+// 	[[0, 1, 2], [1, 3, 2]],
+// 	Colour.YELLOW, Material.matte
+// ) )
+
 // Camera
 scene.camera = new Camera( new Vector(0, -350, 0), new Vector(1,0,0), new Vector(0,0,-1), 20, 20, 1 )
 scene.camera.scene = scene
 
-export default scene
+export default async () => scene
